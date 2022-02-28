6977bd216d950d45185bc80f78d97c84e1c45581
iamr2003 <roughtar@gmail.com>
2022-02-28 18:25:50 -0500

    some progress on little event reader, impeded by learning rust things slowly

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ struct Event{
     priority: i32, //eventually can become a trait/enum or something
     time_start: i64, //going to make everything inclusive for now, dw abt it
     time_end: i64,   //eventually will make start or end exclusive
-    name: String //don't understand strings properly yet
+    name: &'static str //don't understand &'static strs properly yet
 }
 
 impl Event{
-    fn new(p:i32,t_s:i64,t_e:i64,n:String)->Event{
+    fn new(p:i32,t_s:i64,t_e:i64,n:&'static str)->Event{
         Event{
             priority:p,
             time_start:t_s,
@@ -25,16 +25,35 @@ impl Event{
 struct EventRequest{
     priority: i32,
     duration: i64,
-    name:String
+    name:&'static str
+}
+
+impl EventRequest{
+    fn new(n:&'static str,p:i32,d:i64)->EventRequest{
+        EventRequest{
+            priority:p,
+            duration:d,
+            name:n
+        }
+    }
 }
 
 struct Schedule{
     overall_start:i64,
     overall_end:i64,
-    events: Vec<Event>
+    events: Vec<Event>, //should consider different data structures, but this is fine
+    time_used: i64 //the times used overall
 }
 
 impl Schedule{
+    fn new(end:i64)->Schedule{
+        Schedule{
+            overall_start:0,
+            overall_end:end,
+            events:Vec::new(),
+            time_used:0
+        }
+    }
     //cloning everything for now, eventually refs would be preferred
     fn get_event(&self,i:i64)->Option<Event>{
         for e in &self.events{
@@ -44,13 +63,45 @@ impl Schedule{
         }
         None
     }
-    fn add_event(&mut self,e:EventRequest)->bool{
+    fn add_event(&mut self,e:&EventRequest)->bool{
         //don't include priority yet, just insert where possible
         //go through a scan for openings to insert
-        false
+        
+        //if there isn't space for antyhing, skip
+        if self.overall_end-self.overall_start-e.duration <= 0{
+            false
+        }
+        else{
+            //fault with sub heres
+            for i in 0..self.events.len()-1{
+                if self.events[i].time_end -self.events[i+1].time_start > e.duration{
+                    self.events.insert(i,Event{
+                        priority:e.priority,
+                        time_start:self.events[i].time_end+1,
+                        time_end:self.events[i].time_end+1+e.duration,
+                        name:e.name
+                    });
+                    self.time_used+=e.duration;
+                    return true;
+                }
+            }
+            false
+        }
     }
 }
 
 fn main(){
     println!("Hello, world!");
+    let mut sched = Schedule::new(300);
+    assert!(sched.get_event(10).is_none());
+    let running = EventRequest::new("Running",1,10);
+    let sleeping = EventRequest::new("Sleep",1, 50);
+    sched.add_event(&running);
+    sched.add_event(&sleeping);
+    let e_3 = sched.get_event(3);
+    let e_21 = sched.get_event(20);
+
+    //a bit messy, need to think more about best ways
+    assert!(e_3.is_some() && *e_3.unwrap().name == *"Running");
+    assert!(e_21.is_some() && *e_21.unwrap().name == *"Sleep");
 }
